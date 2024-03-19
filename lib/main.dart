2d3db1ba3e67128f8b211334923dafc7181d2d40
Larry <func_dev@126.com>
2024-03-19 15:59:10 +0800

    更新token、channel

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,9 @@ void main() => runApp(MyApp());
 // 填写项目的 App ID，可在声网控制台中生成
 const appId = "d6da9a22cf2c4307a363035a092ad11f";
 // 填写声网控制台中生成的临时 Token
-const token = "007eJxTYIiarNO42XarVc6tpR8XbeH5fJN9YtPWiCUZd+f8OJKkwf5PgSHFLCXRMtHIKDnNKNnE2MA80djM2MDYNNHA0igxxdAwbb7Bz9SGQEYGSfnFLIwMEAjiczIYGhmbmJqZW1gyMAAAdogg/A==";
+const token = "007eJxTYLDyVsmLuGfLoPL6xfZj0Tfnbt9VMFXX9U5txNujL5c92TVfgSHFLCXRMtHIKDnNKNnE2MA80djM2MDYNNHA0igxxdAwrcDuZ2pDICND6XIVFkYGCATxORkMjYxNTM3MLSwZGACGwSGs";
 // 填写频道名
-const channel = "123456";
+const channel = "123456789";
 
 // 应用类
 class MyApp extends StatefulWidget {
