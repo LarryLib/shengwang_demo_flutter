60732cec14da49375ad27075ef6fd3f8b8c24177
Larry <func_dev@126.com>
2024-03-19 14:15:28 +0800

    【pubspec.yaml】: Add `agora_rtc_engine` and `permission_handler`

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import agora_rtc_engine
+import iris_method_channel
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AgoraRtcNgPlugin.register(with: registry.registrar(forPlugin: "AgoraRtcNgPlugin"))
+  IrisMethodChannelPlugin.register(with: registry.registrar(forPlugin: "IrisMethodChannelPlugin"))
 }
